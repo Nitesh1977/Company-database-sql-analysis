32fb69b5b66ca2cc594ff626ee05f1cb25b7c1d4
Nitesh Agarwal <niteshagarwalagarwal518@gmail.com>
2026-04-10 12:45:37 +0530

    Add files via upload

---
+++ data/data.sql
@@ -0,0 +1,12 @@
+use company_db;
+insert into employee (name , department , salary , joining_date) values 
+('Amit', 'IT', 50000, '2022-01-10'),
+('Riya', 'HR', 30000, '2023-03-15'),
+('Mohan', 'Sales', 40000, '2021-07-20'),
+('Amit', 'IT', 60000, '2022-05-12'),
+('Neha', 'HR', 35000, '2023-06-25'),
+('Rahul', 'Sales', 45000, '2020-09-30'),
+('Sohan', 'IT', 70000, '2021-12-01'),
+('Pooja', 'HR', 32000, '2022-11-11'),
+('Karan', 'Sales', 38000, '2023-02-18'),
+('Riya', 'HR', 30000, '2021-08-08'); 
